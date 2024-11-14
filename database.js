3fc261be1cbd0c35a7f17f3aa54a3cc46d1ed2d2
Tonzaly <patchara.anugulsablert@gmail.com>
2024-11-14 23:56:01 +0700

    update

--- database.js
+++ database.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql2');
 
+// const connection = mysql.createConnection({
+//     host: '10.200.200.1',
+//     user: 'navicat_user',
+//     password: 'n0P@22w0rd',
+//     database: 'mtnseviceth',
+//     multipleStatements: true
+// });
+
 const connection = mysql.createConnection({
-    host: '10.200.200.1',
-    user: 'navicat_user',
+    host: 'localhost',
+    user: 'root',
     password: 'n0P@22w0rd',
     database: 'mtnseviceth',
     multipleStatements: true
 });
-
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'n0P@22w0rd',
-//     database: 'mtnseviceth'
-// });
 
 
 
