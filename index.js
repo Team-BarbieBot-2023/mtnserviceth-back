e254e19aeb4c257e3391418d5f1fc9b86a2a67a0
Tonzaly <patchara.anugulsablert@gmail.com>
2024-11-15 23:18:58 +0700

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use('/wormsea', (req, res) => {
+app.use('api/wormsea', (req, res) => {
     res.send('My APIs Wormsea.com');
 });
 
